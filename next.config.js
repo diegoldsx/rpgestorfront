2fc293f6039e0169f80698493b4489d819b112f3
Diego Lopes <diegolopes087@gmail.com>
2025-03-07 12:31:59 -0300

    image optimization

--- next.config.js
+++ next.config.js
@@ -1,54 +1,38 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-	typescript: {
-		ignoreBuildErrors: true,
-	},
+	swcMinify: true, // Usa o SWC para minificação mais rápida
+
 	webpack(config) {
-		// Grab the existing rule that handles SVG imports
-		const fileLoaderRule = config.module.rules.find((rule) =>
-			rule.test?.test?.(".svg")
-		);
+		const fileLoaderRule = config.module.rules.find((rule) => rule.test instanceof RegExp && rule.test.test(".svg"));
 
-		config.module.rules.push(
-			// Reapply the existing rule, but only for svg imports ending in ?url
-			{
-				...fileLoaderRule,
-				test: /\.svg$/i,
-				resourceQuery: /url/, // *.svg?url
-			},
-			// Convert all other *.svg imports to React components
-			{
-				test: /\.svg$/i,
-				issuer: fileLoaderRule.issuer,
-				resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-				use: ["@svgr/webpack"],
-			}
-		);
+		if (fileLoaderRule) {
+			config.module.rules.push(
+				{
+					...fileLoaderRule,
+					test: /\.svg$/i,
+					resourceQuery: /url/, // *.svg?url
+				},
+				{
+					test: /\.svg$/i,
+					issuer: fileLoaderRule.issuer,
+					resourceQuery: { not: [...(fileLoaderRule.resourceQuery?.not || []), /url/] },
+					use: ["@svgr/webpack"],
+				}
+			);
 
-		// Modify the file loader rule to ignore *.svg, since we have it handled now.
-		fileLoaderRule.exclude = /\.svg$/i;
+			fileLoaderRule.exclude = /\.svg$/i;
+		}
 
 		return config;
 	},
 	images: {
+		deviceSizes: [320, 420, 768, 1024, 1200], // Define tamanhos padrão para melhor otimização
+		formats: ["image/avif", "image/webp"], // Usa AVIF e WebP para menor peso
 		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "api.lorem.space",
-			},
-			{
-				protocol: "https",
-				hostname: "lh3.googleusercontent.com",
-			},
-			{
-				protocol: "https",
-				hostname: "a0.muscache.com",
-			},
-			{
-				protocol: "https",
-				hostname: "avatars.githubusercontent.com",
-			},
+			{ protocol: "https", hostname: "api.lorem.space" },
+			{ protocol: "https", hostname: "lh3.googleusercontent.com" },
+			{ protocol: "https", hostname: "a0.muscache.com" },
+			{ protocol: "https", hostname: "avatars.githubusercontent.com" },
 		],
 	},
 };
